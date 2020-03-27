b9040dbe1706b5ce2688596838b23320a9562161
Lydia <snowpeech@gmail.com>
2020-03-27 16:11:12 -0500

    initializing repo

---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+const display = document.querySelector("#display");
+const imageUrl = document.querySelector("#imageURL");
+const topTextIn = document.querySelector("#topText");
+const btmTextIn = document.querySelector("#btmText");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  display.innerHTML = "";
+  createText(topTextIn.value, "top");
+  createCheckImg(imageUrl.value);
+  createText(btmTextIn.value, "btm");
+});
+
+function createText(textIn, position) {
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("meme-text", "border", position);
+  newDiv.innerText = textIn;
+  display.append(newDiv);
+}
+
+function createCheckImg(image) {
+  let newImg = document.createElement("img");
+  newImg.setAttribute("src", image);
+  display.append(newImg);
+}
